b9d2964525c7ec89b29d7f9cd9ddb9ff1efcbc91
qinxi <qinxi@users.noreply.github.com>
2024-10-06 17:11:37 +0800

    custom domain

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,15 +12,11 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Qinxi\'s Workouts Map',
-  siteUrl: 'https://qinxi.github.io/workouts_page/',
+  siteUrl: 'https://workout.qinxi1992.com/',
   logo: 'https://avatars.githubusercontent.com/u/6198010?v=4',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
-    {
-      name: 'Blog',
-      url: 'https://qinxi.github.io/workouts_page/',
-    },
     {
       name: 'About',
       url: 'https://github.com/qinxi/workouts_page/blob/master/README-CN.md',
